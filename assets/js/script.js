2d18884aa9ab61c7683f2769153643864b4149ad
EdgarasSp <dev.applications.esp@gmail.com>
2021-12-21 12:36:23 +0000

    Set up structure of game functions

---
+++ assets/js/script.js
@@ -0,0 +1,35 @@
+function runGame(){
+
+}
+
+function checkAnwser(){
+    
+}
+
+function calculateCorrectAnswer(){
+    
+}
+
+function incrementScore(){
+    
+}
+
+function incrementWrongAnswer(){
+    
+}
+
+function displayAdditionalQuestion(){
+    
+}
+
+function displaySubtractQuestion(){
+    
+}
+
+function displayMultiplyQuestion(){
+    
+}
+
+// function displayDivisionQuestion(){
+    
+// }
